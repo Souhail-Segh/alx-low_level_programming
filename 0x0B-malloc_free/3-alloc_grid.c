8b9ec1075fb0f26300f702d47f5d52cdb65e567c
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-06-14 17:05:29 -0700

    create 2D Matrix

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid - 2D matrix
+ * @width: width
+ * @height: height
+ * Return: 2D matrix if succed, NULL if fail
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **m, i, j;
+
+	i = 0;
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	m = malloc(sizeof(int *) * width);
+	if (m == NULL)
+		return (NULL);
+	while (i < height)
+	{
+		m[i] = malloc(sizeof(int) * height);
+		if (m[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(m[i]);
+
+			free(m);
+			return (NULL);
+		}
+		i++;
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			m[i][j] = 0;
+	}
+
+	return (m);
+}
