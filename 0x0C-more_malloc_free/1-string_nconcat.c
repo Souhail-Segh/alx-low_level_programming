f17e8192596af883a5fffad1af6c1345755789cf
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-06-25 06:37:34 -0700

    concatenation to a specific number

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,34 +19,34 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
-	else
-		while (s1[i] != '\0')
-			i++;
+	while (s1[i] != '\0')
+		i++;
 
 	if (s2 == NULL)
 		s2 = "";
+	while (s2[j] != '\0')
+		j++;
 
-	else
-		while (s2[j] != '\0')
-			j++;
+	if (j > n && n > 0)
+		j = n - 1;
 
-	if (j > n)
-		j = n;
-
-	s = malloc(i + j + 2);
+	s = malloc(i + j + 1);
 	if (s == NULL)
 		return (NULL);
+
 	while (cnt <= i)
 	{
 		s[cnt] = s1[cnt];
 		cnt++;
 	}
+
 	cnt = 0;
 	while (cnt <= j)
 	{
 		s[cnt + i] = s2[cnt];
 		cnt++;
 	}
-	s[i + j + 1] = '\0';
+
+	s[i + cnt] = '\0';
 	return (s);
 }
