44a691bffad2927c05bde25ef2323b6b7dac3dfa
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-06-30 15:01:35 -0700

    free linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_list - free a linked list
+ * @head: adress of first node
+ *
+ * Return: none
+ */
+
+void free_list(list_t *head)
+{
+	list_t temp;
+
+	while (*head != NULL)
+	{
+		temp = *head->next;
+		free(*head);
+		*head = temp;
+	}
+}
