b33e4e2c462d5745711996faedd795e45a8c09ca
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-06-30 15:03:26 -0700

    free linked list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -13,10 +13,11 @@ void free_list(list_t *head)
 {
 	list_t temp;
 
-	while (*head != NULL)
+	while (head != NULL)
 	{
-		temp = *head->next;
-		free(*head);
-		*head = temp;
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
 	}
 }
