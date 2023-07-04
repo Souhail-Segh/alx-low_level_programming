0954c35a9e54e814aa7d4c27851d4e641eb13999
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-07-04 05:33:03 -0700

    Task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,11 +11,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (*head != NULL)
+	if (head == NULL)
+		return;
+	while (*head)
 	{
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
-	*head = NULL;
+	head = NULL;
 }
