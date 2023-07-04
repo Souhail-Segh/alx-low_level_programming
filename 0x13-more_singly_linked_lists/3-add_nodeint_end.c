bd80e7bc4bff7d359c58231c84d80bc6d62b8d29
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-07-04 05:08:13 -0700

    Task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,24 +1,6 @@
 #include "lists.h"
 
 /**
- * str_length - calculate the length of a string
- * @str: string
- *
- * Return: length of a string
- */
-
-int str_length(const char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-
-	return (i);
-}
-
-/**
  * add_nodeint_end - add node at the end of linked list
  * @head: the first node to a linked list
  * @n: the integer value
@@ -28,24 +10,21 @@ int str_length(const char *str)
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	int len;
-	list_t *new, *browser;
+	listint_t *new, *browser;
 
-	new = malloc(sizeof(list_t));
+	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 	{
 		free(new);
 		return (*head);
 	}
-	browser = malloc(sizeof(list_t));
+	browser = malloc(sizeof(listint_t));
 	if (browser == NULL)
 	{
 		free(browser);
 		return (*head);
 	}
-	len = str_length(str);
-	new->len = len;
-	new->str = strdup(str);
+	new->n = n;
 	new->next = NULL;
 	if (*head == NULL)
 	{
