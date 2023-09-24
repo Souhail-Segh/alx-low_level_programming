26b923c4bc660608fe3225340d613c11f0c2e209
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-09-24 13:23:48 -0700

    Task 4 /  1

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -15,11 +15,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
-	index = key_index((unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 
 	for (i = index; ht->array[i]; i++)
 	{
-		if (strcmp(ht->array[i]->key, key)== 0)
+		if (strcmp(ht->array[i]->key, key) == 0)
 		{
 			return (ht->array[i]->value);
 		}
