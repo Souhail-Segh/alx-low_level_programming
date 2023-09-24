6e2f6d538871c5114aa89c4d83bb8c7b0d523eed
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-09-24 13:17:15 -0700

    Task 4/  1

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get value from a key
+ *
+ * @ht: hash table
+ * @key: the key of key/value
+ *
+ * Return: Value, Otherwide NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index, i;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key)== 0)
+		{
+			return (ht->array[i]->value);
+		}
+	}
+	return (NULL);
+}
