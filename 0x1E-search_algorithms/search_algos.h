c29e86d2851bfb80dd9c21b950c60fcd15591bf9
Souhail-Segh <souhail.segh.ds@gmail.com>
2024-01-17 10:11:04 -0800

    update header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,5 +5,6 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+void print_subarray(int *array, size_t start, size_t end);
 
 #endif
